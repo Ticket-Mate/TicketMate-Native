e1388bfa5b22fac9accefdbff5326db681e44ad2
ilay cohen <ilaycohen@gmail.com>
2024-07-24 18:51:51 +0300

    adding nav under the logout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import "react-native-reanimated";
 import { theme } from "@/constants/theme";
 import { PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
 import HomePageNavigation from "@/components/navigation/HomePageNavigation";
+import BottomTabNavigator from "@/components/navigation/BottomTabNavigator";
+import useUser from "@/hooks/useUser";
 
 export default function RootLayout() {
+  const { user } = useUser();
+
   return (
     <PaperProvider theme={theme}>
-      <HomePageNavigation />
+      <NavigationContainer independent={true}>
+        {user ? <BottomTabNavigator /> : <HomePageNavigation />}
+      </NavigationContainer>
     </PaperProvider>
   );
 }
