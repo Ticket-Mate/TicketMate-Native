b1212f234edf6e3a71db401e85061b2bc18cc325
AdiMor1999 <adimor87@gmail.com>
2024-08-28 21:44:40 +0300

    log

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,35 +1,37 @@
+import dayjs from "dayjs";
 
-import dayjs from 'dayjs';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { refreshToken } from "@/api/auth";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { refreshToken } from '@/api/auth';
+export const getAuthToken = async (): Promise<string | undefined> => {
+  const storedUser = await AsyncStorage.getItem("user");
 
+  if (storedUser) {
+    try {
+      const {
+        accessToken,
+        lastRefreshTime,
+        refreshToken: token,
+        refreshTokenInterval,
+      } = JSON.parse(storedUser);
+      console.log("token:  ", accessToken);
+      if (!accessToken) {
+        throw Error("Authorization token is missing");
+      }
 
+      const currentTime = dayjs();
+      const elapsedTime = currentTime.diff(lastRefreshTime);
 
-export const getAuthToken = async (): Promise<string | undefined>  => {
-    const storedUser = await AsyncStorage.getItem('user');
-    
-    if (storedUser) {
-        try {
-            const { accessToken, lastRefreshTime, refreshToken:token, refreshTokenInterval } = JSON.parse(storedUser);
+      if (elapsedTime >= refreshTokenInterval) {
+        const { data: user } = await refreshToken(token);
+        await AsyncStorage.setItem("user", JSON.stringify(user));
+        return user.accessToken;
+      }
 
-            if (!accessToken) {
-                throw Error("Authorization token is missing");
-            }
-
-            const currentTime = dayjs();
-            const elapsedTime = currentTime.diff(lastRefreshTime);
-
-            if (elapsedTime >= refreshTokenInterval) {
-                const { data: user } = await refreshToken(token);
-                await AsyncStorage.setItem('user', JSON.stringify(user));
-                return user.accessToken
-            }
-
-            return accessToken;
-        } catch (error) {
-            throw Error(`Error parsing user AsyncStorage:${error}`);
-        }
+      return accessToken;
+    } catch (error) {
+      throw Error(`Error parsing user AsyncStorage:${error}`);
     }
-    return undefined;
+  }
+  return undefined;
 };
