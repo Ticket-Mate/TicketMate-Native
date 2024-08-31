2e763854da1b0fbc6a6de518b991e6ce9b7609bf
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-08-31 19:05:32 +0300

    fix login logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,34 @@
 import React, { FC } from "react";
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, SafeAreaView } from "react-native";
 import { Text, useTheme } from "react-native-paper";
 
 const Header: FC = () => {
   const { colors } = useTheme();
 
   return (
-    <Text
-      variant="displayMedium"
-      style={[styles.text, { color: colors.primary }]}
-    >
-      TicketMate
-    </Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/images/logo.png')} />
+      <Text variant="displaySmall" style={styles.text}>
+        Ticket Mate
+      </Text>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 25,
+  },
+  logo: {
+    width: 150,
+    height: 80,
+  },
   text: {
     textAlign: "center",
-    marginTop: 100,
-    fontWeight: "700",
+    marginTop: 10,
+    fontWeight: "700",    
   },
 });
 
